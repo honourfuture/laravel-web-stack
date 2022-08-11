521591533f17df00bc0013258b23f8984cbd60cf
honourfuture <honourfuture@163.com>
2022-08-11 11:42:01 +0800

    feat(View/Composer): 修改文案, 修复windows图片类型不识别tmp(https://github.com/Intervention/image/pull/1049)

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -46,11 +46,11 @@
             @endforeach
 
             <div class="submit-tag">
-                <a href="/about">
-                    <i class="linecons-heart"></i>
-                    <span class="tooltip-blue">关于本站</span>
-                    <span class="label label-Primary pull-right hidden-collapsed">♥︎</span>
-                </a>
+{{--                <a href="/about">--}}
+{{--                    <i class="linecons-heart"></i>--}}
+{{--                    <span class="tooltip-blue">关于本站</span>--}}
+{{--                    <span class="label label-Primary pull-right hidden-collapsed">♥︎</span>--}}
+{{--                </a>--}}
             </div>
         </ul>
     </div>
